fcf337a3047d4670e70ac072ed8ea974eadab461
unknown <Anisur Rahman>
2020-08-03 12:20:34 +0200

    First Version of the Repository

---
+++ authapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def restricted(request, *args, **kwargs):
+    return Response(data="Only for logged in user", status=status.HTTP_200_OK)
